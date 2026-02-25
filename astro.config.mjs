8e8c21505e5847f36439c880eb2fbdd89e1c45f6
“Vicodertoten” <victorryelandt2@gmail.com>
2026-02-25 19:24:24 +0100

    azze

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,12 @@ export default defineConfig({
   site: CANONICAL_SITE_URL,
   adapter: netlify(),
   integrations: [],
+  redirects: {
+    '/easy-blog': '/recettes/',
+    '/copie-de-accueil': '/thermomix/',
+    '/gastronomade': '/',
+    '/charte': '/auteur/',
+  },
   image: {
     // Configuration d'optimisation d'images intégrée à Astro 5
     service: {
